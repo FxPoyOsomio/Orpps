0ad69495850fb74c9f626cac094422cc1c4d8833
François-Xavier Poy <francoisxavierpoy@osomio.fr>
2024-10-31 09:52:07 +0100

    charger le header et le footer dans script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Menu burger
     const burgerMenu = document.getElementById("burgerMenu");
     const overlayMenu = document.getElementById("overlayMenu");
 
-    burgerMenu.addEventListener("click", () => {
-        overlayMenu.classList.toggle("active");
-    });
+    if (burgerMenu && overlayMenu) {
+        burgerMenu.addEventListener("click", () => {
+            overlayMenu.classList.toggle("active");
+        });
 
-    overlayMenu.addEventListener("click", (event) => {
-        if (event.target === overlayMenu) {
-            overlayMenu.classList.remove("active");
-        }
-    });
+        overlayMenu.addEventListener("click", (event) => {
+            if (event.target === overlayMenu) {
+                overlayMenu.classList.remove("active");
+            }
+        });
+    }
+
+    // Charger le header
+    fetch("/components/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
+
+    // Charger le footer
+    fetch("/components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
 });
