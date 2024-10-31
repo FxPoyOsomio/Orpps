496ab6a3f8e3fbabe556161cb99bc316df987294
François-Xavier Poy <francoisxavierpoy@osomio.fr>
2024-10-31 10:58:26 +0100

    Réorganiser la logique DOM

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const burgerMenu = document.getElementById("burgerMenu");
-    const overlayMenu = document.getElementById("overlayMenu");
-
-    if (burgerMenu && overlayMenu) { // Vérifie que les éléments existent
-        // Gestion du clic sur le menu burger
-        burgerMenu.addEventListener("click", () => {
-            overlayMenu.classList.toggle("active"); // Ajouter ou retirer la classe "active"
-        });
-
-        // Gestion du clic en dehors du menu pour le fermer
-        overlayMenu.addEventListener("click", (event) => {
-            if (event.target === overlayMenu) {
-                overlayMenu.classList.remove("active"); // Retirer la classe "active"
-            }
-        });
-    } else {
-        console.error("Element(s) not found:", { burgerMenu, overlayMenu });
-    }
-
     // Charger le header
     fetch("../components/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header").innerHTML = data;
+
+            // Une fois le header chargé, accéder aux éléments
+            const burgerMenu = document.getElementById("burgerMenu");
+            const overlayMenu = document.getElementById("overlayMenu");
+
+            if (burgerMenu && overlayMenu) {
+                // Gestion du clic sur le menu burger
+                burgerMenu.addEventListener("click", () => {
+                    overlayMenu.classList.toggle("active"); // Ajouter ou retirer la classe "active"
+                });
+
+                // Gestion du clic en dehors du menu pour le fermer
+                overlayMenu.addEventListener("click", (event) => {
+                    if (event.target === overlayMenu) {
+                        overlayMenu.classList.remove("active"); // Retirer la classe "active"
+                    }
+                });
+            } else {
+                console.error("Element(s) not found:", { burgerMenu, overlayMenu });
+            }
         });
 
     // Charger le footer
