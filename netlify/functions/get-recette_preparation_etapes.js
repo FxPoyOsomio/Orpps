2798fa4050d1559722c5538c70775099dfd275a9
FxPoyOsomio <francoisxavierpoy@osomio.fr>
2024-11-09 18:37:42 +0100

    template recette.html À PEU PRÉS FONCTIONNEL

--- netlify/functions/get-recette_preparation_etapes.js
+++ netlify/functions/get-recette_preparation_etapes.js
@@ -5,7 +5,11 @@ exports.handler = async (event, context) => {
   const baseId = process.env.AIRTABLE_BASE_ID;
   const preparationEtapesTableId = process.env.AIRTABLE__RECETTE_PREPARATION_ETAPES__TABLE_ID;
 
-  const url = `https://api.airtable.com/v0/${baseId}/${preparationEtapesTableId}`;
+  // Vérifiez si un id est passé dans les paramètres d'URL
+  const recordId = event.queryStringParameters.id;
+  const url = recordId
+    ? `https://api.airtable.com/v0/${baseId}/${preparationEtapesTableId}/${recordId}`
+    : `https://api.airtable.com/v0/${baseId}/${preparationEtapesTableId}`;
 
   try {
     const response = await fetch(url, {
@@ -19,9 +23,11 @@ exports.handler = async (event, context) => {
     }
 
     const data = await response.json();
+
+    // Si un id est fourni, encapsuler la réponse dans un tableau
     return {
       statusCode: 200,
-      body: JSON.stringify(data.records)
+      body: JSON.stringify(recordId ? [data] : data.records)
     };
   } catch (error) {
     return { statusCode: 500, body: JSON.stringify({ error: 'Internal Server Error' }) };
